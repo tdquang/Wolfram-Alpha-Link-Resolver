187115913054aee65808468e514118d4e5ab9971
tdquang <dangquang2011@gmail.com>
2016-07-05 00:59:01 -0700

    added comments

--- utils/key.js
+++ utils/key.js
@@ -1,3 +1,3 @@
-// This is the Demo API key from https://github.com/giphy/GiphyAPI and not meant to be used
+// This is the Demo API key from https://github.com/tdquang/Wolfram-Alpha-Link-Resolver and not meant to be used
 // for production traffic.
 module.exports = 'WL9HT9-HQK69A86VA';
